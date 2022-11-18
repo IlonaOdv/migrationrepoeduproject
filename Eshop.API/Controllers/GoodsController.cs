102c9a1065a0431336c5976ba95bbd1197314983
IlonaOdv <116344179+IlonaOdv@users.noreply.github.com>
2022-11-18 14:33:07 +0100

    Revert "LoggingAndFakeRepositoryInProgress"

--- Eshop.API/Controllers/GoodsController.cs
+++ Eshop.API/Controllers/GoodsController.cs
@@ -1,7 +1,5 @@
 ﻿using Eshop.BLL.DTOs;
-using Eshop.DAL.Repositories;
 using Microsoft.AspNetCore.Mvc;
-using System;
 
 namespace Eshop.API.Controllers
 {
@@ -9,74 +7,19 @@ namespace Eshop.API.Controllers
     [Route("[controller]")]
     public class GoodsController : ControllerBase
     {
-        // create Repository that return data (bogus)
-
-        private readonly ILogger logger;
-
-        public GoodsController(ILogger logger)
-        {
-            this.logger = logger;
-        }
-
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<GoodsListDTO>>> GetAllAsync()
-        {
-            logger.LogInformation("All goods to be retrieved.",
-                DateTime.UtcNow.ToLongTimeString());
-
-            var repository = new FakeRepository();
-            var dataFakeRepository = repository.GetDataAsync();
-
-            throw new NotImplementedException();
-        } 
-            
+        public async Task<ActionResult<IEnumerable<GoodsListDTO>>> GetAllAsync() => throw new NotImplementedException();
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<GoodsDetailDTO>> GetByIdAsync(int id)
-        {
-            logger.LogInformation("{id} to be retrieved.",
-                DateTime.UtcNow.ToLongTimeString());
-
-            var repository = new FakeRepository();
-            var dataFakeRepository = repository.GetDataById();
-
-            throw new NotImplementedException();
-        }
+        public async Task<ActionResult<GoodsDetailDTO>> GetByIdAsync(int id) => throw new NotImplementedException();
 
         [HttpPost]
-        public async Task<IActionResult> AddAsync(CreateGoodsDTO data)
-        {
-            logger.LogInformation("Data to be created.",
-                DateTime.UtcNow.ToLongTimeString());
-
-            var repository = new FakeRepository();
-            var dataFakeRepository = repository.CreateData();
-
-            throw new NotImplementedException();
-        }
+        public async Task<IActionResult> AddAsync(CreateGoodsDTO data) => throw new NotImplementedException();
 
         [HttpPut]
-        public async Task<IActionResult> UpdateAsync(GoodsDetailDTO data)
-        {
-            logger.LogInformation("Data to be updated.",
-                DateTime.UtcNow.ToLongTimeString());
-
-            var repository = new FakeRepository();
-            var dataFakeRepository = repository.UpdateData();
-
-            throw new NotImplementedException();
-        }
+        public async Task<IActionResult> UpdateAsync(GoodsDetailDTO data) => throw new NotImplementedException();
 
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteAsync(int id)
-        {
-            logger.LogInformation("{id} to be deleted.",
-                DateTime.UtcNow.ToLongTimeString());
-
-            var repository = new FakeRepository();
-            var dataFakeRepository = repository.DeleteDataById();
-
-            throw new NotImplementedException();
-        }
+        public async Task<IActionResult> DeleteAsync(int id) => throw new NotImplementedException();
     }
 }
--- Eshop.API/Controllers/ProducerController.cs
+++ Eshop.API/Controllers/ProducerController.cs
@@ -1,6 +1,5 @@
 ﻿using Eshop.BLL.DTOs;
 using Microsoft.AspNetCore.Mvc;
-using System;
 
 namespace Eshop.API.Controllers
 {
@@ -8,71 +7,19 @@ namespace Eshop.API.Controllers
     [Route("[controller]")]
     public class ProducerController : ControllerBase
     {
-        private readonly ILogger logger;
-
-        public ProducerController(ILogger logger)
-        {
-            this.logger = logger;
-        }
-
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<ProducerListDTO>>> GetAllAsync()
-        {
-            logger.LogInformation("All producers to be retrieved.",
-                DateTime.UtcNow.ToLongTimeString());
-
-            var repository = new FakeRepository();
-            var data = await repository.GetDataAsync();
-            var data2 = repository.GetData();
-            throw new NotImplementedException();
-        }
+        public async Task<ActionResult<IEnumerable<ProducerListDTO>>> GetAllAsync() => throw new NotImplementedException();
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<ProducerDetailDTO>> GetByIdAsync(int id)
-        {
-            logger.LogInformation("{id} to be retrieved.",
-                DateTime.UtcNow.ToLongTimeString());
-
-            var repository = new FakeRepository();
-            var data = await repository.GetDataAsync();
-            var data2 = repository.GetData();
-            throw new NotImplementedException();
-        }
+        public async Task<ActionResult<ProducerDetailDTO>> GetByIdAsync(int id) => throw new NotImplementedException();
 
         [HttpPost]
-        public async Task<IActionResult> AddAsync(CreateProducerDTO data)
-        {
-            logger.LogInformation("Data to be created.",
-                DateTime.UtcNow.ToLongTimeString());
-
-            var repository = new FakeRepository();
-            var data = await repository.GetDataAsync();
-            var data2 = repository.GetData();
-            throw new NotImplementedException();
-        }
+        public async Task<IActionResult> AddAsync(CreateProducerDTO data) => throw new NotImplementedException();
 
         [HttpPut]
-        public async Task<IActionResult> UpdateAsync(ProducerDetailDTO data)
-        {
-            logger.LogInformation("Data to be updated.",
-                DateTime.UtcNow.ToLongTimeString());
-
-            var repository = new FakeRepository();
-            var data = await repository.GetDataAsync();
-            var data2 = repository.GetData();
-            throw new NotImplementedException();
-        }
+        public async Task<IActionResult> UpdateAsync(ProducerDetailDTO data) => throw new NotImplementedException();
 
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteAsync(int id)
-        {
-            logger.LogInformation("{id} to be deleted.",
-                DateTime.UtcNow.ToLongTimeString());
-
-            var repository = new FakeRepository();
-            var data = await repository.GetDataAsync();
-            var data2 = repository.GetData();
-            throw new NotImplementedException();
-        }
+        public async Task<IActionResult> DeleteAsync(int id) => throw new NotImplementedException();
     }
 }
